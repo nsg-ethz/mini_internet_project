53f13da35954deff25be5f8839456ae11905b280
Thomas Holterbach <thomasholterbach@gmail.com>
2019-12-04 23:33:22 +0100

    change proxy hostname

--- platform/setup/container_setup.sh
+++ platform/setup/container_setup.sh
@@ -33,7 +33,8 @@ for ((k=0;k<group_numbers;k++)); do
         location="${DIRECTORY}"/groups/g"${group_number}"
         # start ssh container
         docker run -itd --net='none'  --name="${group_number}""_ssh" \
-          -v "${location}"/goto.sh:/root/goto.sh --privileged thomahol/d_ssh
+          -v "${location}"/goto.sh:/root/goto.sh --privileged thomahol/d_ssh \
+          --hostname "g${group_number}-proxy"
 
         for ((i=0;i<n_routers;i++)); do
             router_i=(${routers[$i]})
