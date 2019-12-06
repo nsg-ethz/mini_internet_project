16a336fa17722b59abf017e6a494f524356fc14e
Thomas Holterbach <thomasholterbach@gmail.com>
2019-12-06 18:02:55 +0100

    added host config

--- platform/setup/host_links_setup.sh
+++ platform/setup/host_links_setup.sh
@@ -20,7 +20,7 @@ for ((k=0;k<group_numbers;k++)); do
     group_k=(${groups[$k]})
     group_number="${group_k[0]}"
     group_as="${group_k[1]}"
-    group_config="${group_k[1]}"
+    group_config="${group_k[2]}"
 
     if [ "${group_as}" != "IXP" ];then
 
@@ -53,7 +53,7 @@ for ((k=0;k<group_numbers;k++)); do
 
                 # set default ip address and default gw in host
                 if [ "$group_config" == "Config" ]; then
-                    echo "docker exec -d "${group_number}"_"${rname}"host ifconfig "${rname}"router "${subnet_host}" up" >> "${DIRECTORY}"/groups/ip_setup.sh
+                    echo "docker exec -d "${group_number}"_"${rdname}"host ifconfig "${rname}"router "${subnet_host}" up" >> "${DIRECTORY}"/groups/ip_setup.sh
                     echo "docker exec -d "${group_number}"_"${rname}"host ip route add default via "${subnet_router%/*} >> "${DIRECTORY}"/groups/ip_setup.sh
                 fi
             fi
