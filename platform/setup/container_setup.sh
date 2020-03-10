8ea6004a08eadc6f0e54789218f55a5338759bec
Thomas Holterbach <thomasholterbach@gmail.com>
2020-03-10 12:42:28 +0100

    fix in the as connection file

--- platform/setup/container_setup.sh
+++ platform/setup/container_setup.sh
@@ -43,7 +43,9 @@ for ((k=0;k<group_numbers;k++)); do
             -v "${location}"/goto.sh:/root/goto.sh --privileged \
             --cpus=2 --pids-limit 100 --hostname="g${group_number}-proxy" \
             -v /etc/timezone:/etc/timezone:ro \
-            -v /etc/localtime:/etc/localtime:ro thomahol/d_ssh
+            -v /etc/localtime:/etc/localtime:ro \
+            -v "${DIRECTORY}"/config/welcoming_message.txt:/etc/motd \
+            thomahol/d_ssh
 
     	# start switches
     	for ((l=0;l<n_l2_switches;l++)); do
