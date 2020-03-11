ce3a1890e499e6d9e298f423a414b0d1251e253b
Thomas Holterbach <thomasholterbach@gmail.com>
2020-03-11 18:31:43 +0100

    remove strange interfaces

--- platform/docker_images/router/looking_glass.sh
+++ platform/docker_images/router/looking_glass.sh
@@ -11,4 +11,9 @@ do
   # save router config
   vtysh -c 'write'
 
+  for intf_name in $(vtysh -c 'show interface brief' | grep _c | cut -f 1 -d ' '); do
+      vtysh -c "conf t" -c "interface $intf_name" -c "shutdown" -c "exit" -c "no interface $intf_name"
+  done
+
+
 done
