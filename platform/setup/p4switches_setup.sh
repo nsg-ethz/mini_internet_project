03689431b32bf74fc9936141b69deba9e7fd9359
Thomas Holterbach <thomasholterbach@gmail.com>
2020-11-16 00:47:31 +0100

    added cpu links

--- platform/setup/p4switches_setup.sh
+++ platform/setup/p4switches_setup.sh
@@ -52,18 +52,22 @@ for ((k=0;k<group_numbers;k++)); do
                 subnet_p4cpu="$(subnet_p4cpu "${group_number}" "p4router" "${i}")"
 
                 ./setup/ovs-docker.sh add-port "${br_name_cpu}" "switch-cpu" \
-                "${group_number}""_""${rname}""router" --ipaddress="${subnet_p4cpu}"
+                "${group_number}""_""${rname}""router"
+
+                subnet_p4api_controller="$(subnet_p4api "${group_number}" "controller" "${i}")"
+
+                echo "ip link add ${group_number}-$rname-api type veth peer name g${group_number}_$rname-api" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ip link set dev ${group_number}-$rname-api up" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ip link set dev g${group_number}_$rname-api up" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ovs-vsctl add-port "${br_name_api}" g${group_number}_$rname-api" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ip addr add "$subnet_p4api_controller" dev ${group_number}-$rname-api" >> "${DIRECTORY}"/groups/ip_setup.sh
+
+                echo "ip link add ${group_number}-$rname-cpu type veth peer name g${group_number}_$rname-cpu" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ip link set dev ${group_number}-$rname-cpu up" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ip link set dev g${group_number}_$rname-cpu up" >> "${DIRECTORY}"/groups/ip_setup.sh
+                echo "ovs-vsctl add-port "${br_name_cpu}" g${group_number}_$rname-cpu" >> "${DIRECTORY}"/groups/ip_setup.sh
             fi
 
-            echo "ip link add ${group_number}-$rname-api type veth peer name g${group_number}_$rname-api" >> "${DIRECTORY}"/groups/ip_setup.sh
-            echo "ip link set dev ${group_number}-$rname-api up" >> "${DIRECTORY}"/groups/ip_setup.sh
-            echo "ip link set dev g${group_number}_$rname-api up" >> "${DIRECTORY}"/groups/ip_setup.sh
-            echo "ovs-vsctl add-port "${br_name_api}" g${group_number}_$rname-api" >> "${DIRECTORY}"/groups/ip_setup.sh
-
-            echo "ip link add ${group_number}-$rname-cpu type veth peer name g${group_number}_$rname-cpu" >> "${DIRECTORY}"/groups/ip_setup.sh
-            echo "ip link set dev ${group_number}-$rname-cpu up" >> "${DIRECTORY}"/groups/ip_setup.sh
-            echo "ip link set dev g${group_number}_$rname-cpu up" >> "${DIRECTORY}"/groups/ip_setup.sh
-            echo "ovs-vsctl add-port "${br_name_cpu}" g${group_number}_$rname-cpu" >> "${DIRECTORY}"/groups/ip_setup.sh
         done
     fi
 done
