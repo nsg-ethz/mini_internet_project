3eb3d9b46609a30c3d354266970804353b0f45c5
Thomas Holterbach <thomasholterbach@gmail.com>
2020-11-16 14:23:59 +0100

    added script to clean up p4 things

---
+++ platform/cleanup/p4switches_cleanup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+DIRECTORY="$1"
+
+readarray groups < "${DIRECTORY}"/config/AS_config.txt
+group_numbers=${#groups[@]}
+
+for ((k=0;k<group_numbers;k++)); do
+    group_k=(${groups[$k]})
+    group_number="${group_k[0]}"
+    group_as="${group_k[1]}"
+    group_layer2_switches="${group_k[5]}"
+    group_layer2_hosts="${group_k[6]}"
+    group_layer2_links="${group_k[7]}"
+
+    if [ "${group_as}" != "IXP" ];then
+
+        readarray routers < "${DIRECTORY}"/config/$group_router_config
+        n_routers=${#routers[@]}
+
+        echo -n "-- --if-exists del-br ${group_number}-p4api" >> "${DIRECTORY}"/ovs_command.txt
+
+    fi
+done
