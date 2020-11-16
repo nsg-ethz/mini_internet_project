b1cda5263351e30aa444ec390e78134fb9315157
Thomas Holterbach <thomasholterbach@gmail.com>
2020-11-16 14:29:26 +0100

    added script to clean up p4 things

--- platform/cleanup/cleanup.sh
+++ platform/cleanup/cleanup.sh
@@ -26,6 +26,7 @@ echo -n "ovs-vsctl " > ovs_command.txt
 ./cleanup/dns_cleanup.sh "${DIRECTORY}"
 ./cleanup/ssh_cleanup.sh "${DIRECTORY}"
 ./cleanup/vpn_cleanup.sh "${DIRECTORY}"
+./cleanup/p4switches_cleanup.sh "${DIRECTORY}"
 
 
 bash  < ovs_command.txt || true
