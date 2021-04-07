e91381704d0134bf2234a47791a0a3005fe14531
KTrel <thomasholterbach@gmail.com>
2021-04-07 19:16:00 +0200

    added script to keep track of the history configs

---
+++ platform/utils/history_config.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+gitdir=/home/thomas/configs_history
+rm -rf $gitdir
+
+sudo -H -u thomas git clone git@gitlab.ethz.ch:nsg/lectures/lec_commnet/projects/2021/routing_project/configs_history.git $gitdir
+
+while true
+do
+    for ((group=3;group<=10;group++)); do
+        if [[ ! -d "$gitdir/g$group" ]]
+        then
+           sudo -H -u thomas mkdir $gitdir/g$group
+        fi
+
+        for r in BROO NEWY CHAR PITT DETR CHIC STLO NASH; do
+            cp /home/thomas/mini_internet_project/platform/groups/g$group/$r/frr.conf $gitdir/g$group/$r.txt
+            chown thomas:thomas $gitdir/g$group/$r.txt
+            sudo -H -u thomas git -C $gitdir add g$group/$r.txt
+        done
+    done
+
+    for ((group=23;group<=30;group++)); do
+        if [[ ! -d "$gitdir/g$group" ]]
+        then
+            sudo -H -u thomas mkdir $gitdir/g$group
+        fi
+
+        for r in BROO NEWY CHAR PITT DETR CHIC STLO NASH; do
+            cp /home/thomas/mini_internet_project/platform/groups/g$group/$r/frr.conf $gitdir/g$group/$r.txt
+            chown thomas:thomas $gitdir/g$group/$r.txt
+            sudo -H -u thomas git -C $gitdir add g$group/$r.txt
+        done
+    done
+
+    for ((group=43;group<=50;group++)); do
+        if [[ ! -d "$gitdir/g$group" ]]
+        then
+            sudo -H -u thomas mkdir $gitdir/g$group
+        fi
+
+        for r in BROO NEWY CHAR PITT DETR CHIC STLO NASH; do
+            cp /home/thomas/mini_internet_project/platform/groups/g$group/$r/frr.conf $gitdir/g$group/$r.txt
+            chown thomas:thomas $gitdir/g$group/$r.txt
+            sudo -H -u thomas git -C $gitdir add g$group/$r.txt
+        done
+    done
+
+    for ((group=63;group<=70;group++)); do
+        if [[ ! -d "$gitdir/g$group" ]]
+        then
+            sudo -H -u thomas mkdir $gitdir/g$group
+        fi
+
+        for r in BROO NEWY CHAR PITT DETR CHIC STLO NASH; do
+            cp /home/thomas/mini_internet_project/platform/groups/g$group/$r/frr.conf $gitdir/g$group/$r.txt
+            chown thomas:thomas $gitdir/g$group/$r.txt
+            sudo -H -u thomas git -C $gitdir add g$group/$r.txt
+        done
+    done
+
+    for ((group=83;group<=90;group++)); do
+        if [[ ! -d "$gitdir/g$group" ]]
+        then
+            sudo -H -u thomas mkdir $gitdir/g$group
+        fi
+
+        for r in BROO NEWY CHAR PITT DETR CHIC STLO NASH; do
+            cp /home/thomas/mini_internet_project/platform/groups/g$group/$r/frr.conf $gitdir/g$group/$r.txt
+            chown thomas:thomas $gitdir/g$group/$r.txt
+            sudo -H -u thomas git -C $gitdir add g$group/$r.txt
+        done
+    done
+
+    for ((group=103;group<=110;group++)); do
+        if [[ ! -d "$gitdir/g$group" ]]
+        then
+            mkdir $gitdir/g$group
+        fi
+
+        for r in BROO NEWY CHAR PITT DETR CHIC STLO NASH; do
+            cp /home/thomas/mini_internet_project/platform/groups/g$group/$r/frr.conf $gitdir/g$group/$r.txt
+            chown thomas:thomas $gitdir/g$group/$r.txt
+            sudo -H -u thomas git -C $gitdir add g$group/$r.txt
+        done
+    done
+
+    d=$(date +'%m/%d/%Y-%Hh%Mm%Ss')
+    sudo -H -u thomas git -C $gitdir commit -m "Config $d"
+    sudo -H -u thomas git -C $gitdir push
+
+    sleep 60
+done
