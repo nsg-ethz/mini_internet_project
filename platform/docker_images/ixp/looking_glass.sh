a2e91b5a0aa4223b75e81000228ab65a9e3e6f74
KTrel <thomasholterbach@gmail.com>
2021-04-08 18:28:05 +0200

    added looking glass script for ixp container

---
+++ platform/docker_images/ixp/looking_glass.sh
@@ -0,0 +1,11 @@
+while true
+do
+  duration=$(shuf -i300-900 -n1)
+
+  for i in {0..$duration..30}; do
+    date "+%FT%T" > /home/looking_glass.txt
+    vtysh -c 'show ip bgp' >> /home/looking_glass.txt
+    vtysh -c 'show ip bgp json' > /home/looking_glass_json.txt
+    sleep 30
+  done
+done
