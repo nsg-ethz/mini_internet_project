55037595d25ee29ac44c86be825ec74b2637212d
Martin Vahlensieck <martinva@student.ethz.ch>
2022-03-17 13:26:43 +0100

    Be more restrictive for the krill webserver key

--- platform/setup/rpki_setup.sh
+++ platform/setup/rpki_setup.sh
@@ -35,7 +35,7 @@ for ((j=0;j<n_krill_containers;j++)); do
             container_name="${container_data[1]}"
 
             # Enable ssh port forwarding to krill webserver via ssh proxy container (and only port forwarding)  
-            docker exec ${group_number}_ssh bash -c "echo 'no-pty,command=\"/bin/false\" $(cat groups/rpki/id_rsa_krill_webserver.pub)' >> ~/.ssh/authorized_keys"
+            docker exec ${group_number}_ssh bash -c "echo 'restrict,port-forwarding,command=\"/bin/false\" $(cat groups/rpki/id_rsa_krill_webserver.pub)' >> ~/.ssh/authorized_keys"
             
             # Setup Certificate Authority and predefined ROAs
             docker exec $container_name /bin/bash /home/setup.sh
