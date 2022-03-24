58b6b201b492cfc7cf3a3184a30348c6151767ab
Thomas Holterbach <thomasholterbach@gmail.com>
2022-03-24 02:10:50 +0100

    filters script fix

--- platform/utils/iptables/filters.sh
+++ platform/utils/iptables/filters.sh
@@ -14,7 +14,7 @@ iptables -t mangle $action PREROUTING -p tcp ! --syn -m conntrack --ctstate NEW 
 # Block Uncommon MSS Values
 iptables -t mangle $action PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpmss ! --mss 536:65535 -j DROP
 
-# Block Packets With Bogus TCP Flags
+# # Block Packets With Bogus TCP Flags
 iptables -t mangle $action PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
 iptables -t mangle $action PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
 iptables -t mangle $action PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
@@ -23,33 +23,33 @@ iptables -t mangle $action PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
 iptables -t mangle $action PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
 iptables -t mangle $action PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
 
-# Reject connections if host has more than 10 established connections
-iptables $action INPUT -p tcp -dport $WEBSERVER_PORT -m connlimit --connlimit-above 10 -j REJECT --reject-with tcp-reset
-iptables $action INPUT -p tcp -dport $KRILL_PORT -m connlimit --connlimit-above 10 -j REJECT --reject-with tcp-reset
+# # Reject connections if host has more than 10 established connections
+iptables $action INPUT -p tcp --dport $WEBSERVER_PORT -m connlimit --connlimit-above 10 -j REJECT --reject-with tcp-reset
+iptables $action INPUT -p tcp --dport $KRILL_PORT -m connlimit --connlimit-above 10 -j REJECT --reject-with tcp-reset
 
-# Limits the new TCP connections that a client can establish per second
-iptables $action INPUT -p tcp -dport $WEBSERVER_PORT -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
-iptables $action INPUT -p tcp -dport $WEBSERVER_PORT -m conntrack --ctstate NEW -j DROP
-iptables $action INPUT -p tcp -dport $KRILL_PORT -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
-iptables $action INPUT -p tcp -dport $KRILL_PORT -m conntrack --ctstate NEW -j DROP
+# # Limits the new TCP connections that a client can establish per second
+iptables $action INPUT -p tcp --dport $WEBSERVER_PORT -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
+iptables $action INPUT -p tcp --dport $WEBSERVER_PORT -m conntrack --ctstate NEW -j DROP
+iptables $action INPUT -p tcp --dport $KRILL_PORT -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
+iptables $action INPUT -p tcp --dport $KRILL_PORT -m conntrack --ctstate NEW -j DROP
 
-# This rule blocks fragmented packets
+# # This rule blocks fragmented packets
 iptables -t mangle $action PREROUTING -f -j DROP
 
-# This limits incoming TCP RST packets to mitigate TCP RST floods
+# # This limits incoming TCP RST packets to mitigate TCP RST floods
 iptables $action INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT 
 iptables $action INPUT -p tcp --tcp-flags RST RST -j DROP
 
-# SYNPROXY
-iptables -t raw $action PREROUTING -p tcp -dport $WEBSERVER_PORT -m tcp --syn -j CT --notrack 
-iptables $action INPUT -p tcp -dport $WEBSERVER_PORT -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
-iptables -t raw $action PREROUTING -p tcp -dport $KRILL_PORT -m tcp --syn -j CT --notrack 
-iptables $action INPUT -p tcp -dport $KRILL_PORT -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460 
+# # SYNPROXY
+iptables -t raw $action PREROUTING -p tcp --dport $WEBSERVER_PORT -m tcp --syn -j CT --notrack 
+iptables $action INPUT -p tcp --dport $WEBSERVER_PORT -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460
+iptables -t raw $action PREROUTING -p tcp --dport $KRILL_PORT -m tcp --syn -j CT --notrack 
+iptables $action INPUT -p tcp --dport $KRILL_PORT -m tcp -m conntrack --ctstate INVALID,UNTRACKED -j SYNPROXY --sack-perm --timestamp --wscale 7 --mss 1460 
 iptables $action INPUT -m conntrack --ctstate INVALID -j DROP
 
-### SSH brute-force protection ### 
+# ### SSH brute-force protection ### 
 # read mini-Internet configs.
-readarray groups < $PLATFORM_DIR/config/AS_config.txt
+readarray groups < $CONFIG_DIR/AS_config.txt
 group_numbers=${#groups[@]}
 
 # Copy routers config for every group.
@@ -59,12 +59,13 @@ for ((k=0;k<group_numbers;k++)); do
     group_as="${group_k[1]}"
     
     if [ "${group_as}" != "IXP" ];then
-
+        echo 'hello'
         iptables $action INPUT -p tcp --dport $(($SSH_PORT_BASE+$group_number)) -m conntrack --ctstate NEW -m recent --set 
         iptables $action INPUT -p tcp --dport $(($SSH_PORT_BASE+$group_number)) -m conntrack --ctstate NEW -m recent --update --seconds 60 --hitcount 10 -j DROP  
-    done
+    fi
+done
 
-### Protection against port scanning ### 
+# ### Protection against port scanning ### 
 iptables -N port-scanning 
 iptables $action port-scanning -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s --limit-burst 2 -j RETURN 
 iptables $action port-scanning -j DROP
