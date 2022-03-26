e3f014b6f8a63b724014676038da8deabe90376f
Thomas Holterbach <thomahol@ethz.ch>
2022-03-26 01:47:30 +0100

    last minute changes

--- platform/setup/save_configs.sh
+++ platform/setup/save_configs.sh
@@ -96,7 +96,7 @@ for ((k=0;k<n_groups;k++)); do
 
     echo "zip \${dirname}.zip \${dirname}/*" >> "${file_loc}"
     echo "echo \"Download zip file:\"" >> "${file_loc}"
-    echo "echo \"    scp -P $((2000 + ${group_number})) root@$(hostname --fqdn):\${dirname}.zip .\"" >> "${file_loc}"
+    echo "echo \"    scp -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname}.zip .\"" >> "${file_loc}"
     echo "echo \"Overwrite the config folder in the current directory:\"" >> "${file_loc}"
-    echo "echo \"    scp -r -P $((2000 + ${group_number})) root@$(hostname --fqdn):\${dirname} config\"" >> "${file_loc}"
+    echo "echo \"    scp -r -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname} config\"" >> "${file_loc}"
 done
