7ff4b7e2396b7b8c03b9e4dbf60d0d13cf6c3754
Thomas Holterbach <thomasholterbach@gmail.com>
2022-05-05 15:08:00 +0200

    added sleep 60 to let RPKI starts properly

--- platform/startup.sh
+++ platform/startup.sh
@@ -230,6 +230,9 @@ time ./setup/mpls_setup.sh "${DIRECTORY}"
 echo ""
 echo ""
 
+echo "Waiting 60sec for RPKI CA and proxy to startup.."
+sleep 60
+
 echo "rpki_setup.sh $(($(date +%s%N)/1000000))" >> "${DIRECTORY}"/log.txt
 echo "rpki_setup.sh: "
 time ./setup/rpki_setup.sh "${DIRECTORY}"
