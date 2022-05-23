f45d8df920adc6b1d12450aae074bee29cb4933c
Thomas Holterbach <thomasholterbach@gmail.com>
2022-05-23 13:08:24 +0200

    adapted the images for restore config script

--- platform/setup/container_setup.sh
+++ platform/setup/container_setup.sh
@@ -64,7 +64,7 @@ for ((k=0;k<group_numbers;k++)); do
                 -v "${location}"/goto.sh:/root/goto.sh  \
                 -v "${location}"/save_configs.sh:/root/save_configs.sh \
                 -v "${location}"/restore_configs.sh:/root/restore_configs.sh \
-                -v "${location}"/restore_configs.sh:/root/restore_configs.sh \
+                -v "${location}"/restart_ospfd.sh:/root/restart_ospfd.sh \
                 -v /etc/timezone:/etc/timezone:ro \
                 -v /etc/localtime:/etc/localtime:ro \
                 -v "${DIRECTORY}"/config/welcoming_message.txt:/etc/motd:ro \
