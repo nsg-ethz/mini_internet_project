9c1e6215fd56f792973b05c72a6b693f497f80e0
Thomas Holterbach <thomahol@ethz.ch>
2022-06-07 12:50:15 +0200

    Update configure_as.sh

--- platform/utils/autoconfiguration/configure_as.sh
+++ platform/utils/autoconfiguration/configure_as.sh
@@ -1,17 +1,30 @@
-# This script is used to configure an ASes that was originally not pre-configure (NoConfig)
+# This script is used to configure ASes that were originally not preconfigured (NoConfig flag)
 # The Layer2 network is not configured, only the L3 network as well as the hosts
 
-for group_number in 9 29 49 69 108
+### TO UPDATE ###
+# this variable is the absolute path to the platform directory.
+PLATFORM_DIR=
+# this variable includes all the AS number that need to be configured.
+WRITE_THE_ASN_TO_CONFIGURE_HERE=
+# this variable contains all the router names that need to be configured.
+ROUTER_NAMES=
+
+for group_number in WRITE_THE_ASN_TO_CONFIGURE_HERE
 do
     rid=1
-    for router_name in BROO NEWY CHAR PITT DETR CHIC STLO NASH
+    # This loop should iterate over the router, starting from lower ID to higher ID.
+    for router_name in ROUTER_NAMES
     do
-        docker cp groups/g${group_number}/${router_name}/init_full_conf.sh ${group_number}_${router_name}router:/home/
+        chmod 755 $PLATFORM_DIR/groups/g${group_number}/${router_name}/init_rpki_conf.sh
+        docker cp $PLATFORM_DIR/groups/g${group_number}/${router_name}/init_full_conf.sh ${group_number}_${router_name}router:/home/
+        docker cp $PLATFORM_DIR/groups/g${group_number}/${router_name}/init_rpki_conf.sh ${group_number}_${router_name}router:/home/
         docker exec -it ${group_number}_${router_name}router ./home/init_full_conf.sh
+        docker exec -it ${group_number}_${router_name}router ./home/init_rpki_conf.sh
 
-        docker exec -it ${group_number}_${router_name}host ifconfig ${router_name}router ${group_number}.$((100+$rid)).0.1/24
+        docker exec -it ${group_number}_${router_name}host ip address add ${group_number}.$((100+$rid)).0.1/24 dev ${router_name}router 
         docker exec -it ${group_number}_${router_name}host ip route add default via ${group_number}.$((100+$rid)).0.2
 
         rid=$(($rid+1))
+        echo $group_number $router_name
     done
 done
