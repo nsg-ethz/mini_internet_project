74c22b16dc808e64480c292aad757b23712f7c94
CommNet Team <comm-net@ethz.ch>
2024-03-22 17:10:15 +0100

    Update save_configs tooltip.

--- platform/setup/save_configs.sh
+++ platform/setup/save_configs.sh
@@ -6,7 +6,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-DIRECTORY="$1"
+DIRECTORY=$(readlink -f $1)
 source "${DIRECTORY}"/config/subnet_config.sh
 
 
@@ -45,14 +45,16 @@ for ((k=0;k<n_groups;k++)); do
     # Prepare group files.
     # ====================
     # Save config.
-    echo "#!/bin/bash" > $file_loc
-    echo "" >> $file_loc
-    echo 'dirname=configs_${1:-$(date +%m-%d-%Y_%H-%M-%S)}' >> $file_loc
-    echo "mkdir -p \$dirname" >> $file_loc
-    echo "" >> $file_loc
-    echo '# Arguments: filename, subnet, command' >> $file_loc
-    echo 'save() { eval "ssh -q -o StrictHostKeyChecking=no root@${2%???} ${@:3} > $1" ; }' >> $file_loc
-    echo "" >> $file_loc
+    {
+        echo "#!/bin/bash"
+        echo ""
+        echo 'dirname=configs_${1:-$(date +%m-%d-%Y_%H-%M-%S)}'
+        echo "mkdir -p \$dirname"
+        echo ""
+        echo '# Arguments: filename, subnet, command'
+        echo 'save() { eval "ssh -q -o StrictHostKeyChecking=no root@${2%???} ${@:3} > $1" ; }'
+        echo ""
+    } > $file_loc
 
     # Restore config.
     cp "${DIRECTORY}/setup/restore_configs.sh" "${restore_loc}"
@@ -96,36 +98,38 @@ for ((k=0;k<n_groups;k++)); do
             l2_id[$l2_name]=$i
         fi
 
-        echo "echo ${rname} router" >> $file_loc
-        echo "mkdir -p $savedir" >> $file_loc
+        {
+            echo "echo ${rname} router"
+            echo "mkdir -p $savedir"
 
-        # Router
-        echo "save $savedir/router.conf      $subnet_router \\\"vtysh -c \\'sh run\\'\\\"" >> $file_loc
-        echo "save $savedir/router.rib.json  $subnet_router \\\"vtysh -c \\'sh ip route json\\'\\\" \\| grep -v uptime" >> $file_loc
-        if [ "${rcmd}" == "linux" ]; then
-            # If we have linux access, we may also configure tunnels, so store that output.
+            # Router
+            echo "save $savedir/router.conf      $subnet_router \\\"vtysh -c \\'sh run\\'\\\""
+            echo "save $savedir/router.rib.json  $subnet_router \\\"vtysh -c \\'sh ip route json\\'\\\" \\| grep -v uptime"
+            if [ "${rcmd}" == "linux" ]; then
+                # If we have linux access, we may also configure tunnels, so store that output.
 
-            # Add tunnels and ipv6 routes.
-            echo "save $savedir/router.rib6.json $subnet_router \\\"vtysh -c \\'sh ipv6 route json\\'\\\" \\| grep -v uptime" >> $file_loc
-            echo "save $savedir/router.tunnels   $subnet_router ip tunnel show" >> $file_loc
-        fi
-
-        # Host
-        echo "echo ${rname} host" >> $file_loc
-        echo "save $savedir/host.ip         $subnet_host ip addr" >> $file_loc
-        echo "save $savedir/host.route      $subnet_host ip route" >> $file_loc
-        echo "save $savedir/host.route6     $subnet_host ip -6 route" >> $file_loc
-
-        # If the host runs routinator, save routinator cache.
-        htype=$(echo $property2 | cut -d ':' -f 1)
-        dname=$(echo $property2 | cut -d ':' -f 2)
-        if [[ ! -z "${dname}" ]]; then
-            if [[ "${htype}" == *"routinator"* ]]; then
-                # echo "save $savedir/host.rpki_cache $subnet_host \"/usr/local/bin/routinator -qq update \; tar -czC /root/.rpki-cache repository\"" >> $file_loc
-                # Without update to speed up things.
-                echo "save $savedir/host.rpki_cache $subnet_host \"tar -czC /root/.rpki-cache repository\"" >> $file_loc
+                # Add tunnels and ipv6 routes.
+                echo "save $savedir/router.rib6.json $subnet_router \\\"vtysh -c \\'sh ipv6 route json\\'\\\" \\| grep -v uptime"
+                echo "save $savedir/router.tunnels   $subnet_router ip tunnel show"
             fi
-        fi
+
+            # Host
+            echo "echo ${rname} host"
+            echo "save $savedir/host.ip         $subnet_host ip addr"
+            echo "save $savedir/host.route      $subnet_host ip route"
+            echo "save $savedir/host.route6     $subnet_host ip -6 route"
+
+            # If the host runs routinator, save routinator cache.
+            htype=$(echo $property2 | cut -d ':' -f 1)
+            dname=$(echo $property2 | cut -d ':' -f 2)
+            if [[ ! -z "${dname}" ]]; then
+                if [[ "${htype}" == *"routinator"* ]]; then
+                    # echo "save $savedir/host.rpki_cache $subnet_host \"/usr/local/bin/routinator -qq update \; tar -czC /root/.rpki-cache repository\""
+                    # Without update to speed up things.
+                    echo "save $savedir/host.rpki_cache $subnet_host \"tar -czC /root/.rpki-cache repository\""
+                fi
+            fi
+        } >> $file_loc
 
         # Prepare restore_configs.sh and restart_osfd.sh for routers
         # TODO: include hosts.
@@ -138,52 +142,62 @@ for ((k=0;k<n_groups;k++)); do
         echo "fi" | tee -a ${restart_ospfd} ${restore_loc} > /dev/null
     done
 
-    for ((l=0;l<n_l2_switches;l++)); do
-        switch_l=(${l2_switches[$l]})
-        l2_name="${switch_l[0]}"
-        sname="${switch_l[1]}"
-        subnet=$(subnet_sshContainer_groupContainer "${group_number}" "${l2_id[$l2_name]}" "${l2_cur[$l2_name]}" "switch")
-        savedir="\${dirname}/$sname"
+    {
+        for ((l=0;l<n_l2_switches;l++)); do
+            switch_l=(${l2_switches[$l]})
+            l2_name="${switch_l[0]}"
+            sname="${switch_l[1]}"
+            subnet=$(subnet_sshContainer_groupContainer "${group_number}" "${l2_id[$l2_name]}" "${l2_cur[$l2_name]}" "switch")
+            savedir="\${dirname}/$sname"
 
-        echo "echo ${sname}" >> $file_loc
-        echo "mkdir -p $savedir" >> $file_loc
-        echo "save $savedir/switch.db      $subnet \"ovsdb-client backup\"" >> $file_loc
-        echo "save $savedir/switch.summary $subnet \"ovs-vsctl show\"" >> $file_loc
-
-        l2_cur[$l2_name]=$((${l2_cur[$l2_name]}+1))
-    done
-
-    for ((l=0;l<n_l2_hosts;l++)); do
-        host_l=(${l2_hosts[$l]})
-        hname="${host_l[0]}"
-        if [[ "$hname" != *VPN* ]];then
-            l2_name="${host_l[2]}"
-            subnet=$(subnet_sshContainer_groupContainer "${group_number}" "${l2_id[$l2_name]}" "${l2_cur[$l2_name]}" "L2-host")
-            savedir="\${dirname}/${hname}"
-
-            echo "echo ${hname}" >> $file_loc
-            echo "mkdir -p $savedir" >> $file_loc
-            echo "save $savedir/host.ip     $subnet \"ip addr\"" >> $file_loc
-            echo "save $savedir/host.route  $subnet \"ip route\"" >> $file_loc
-            echo "save $savedir/host.route6 $subnet \"ip -6 route\"" >> $file_loc
+            echo "echo ${sname}"
+            echo "mkdir -p $savedir"
+            echo "save $savedir/switch.db      $subnet \"ovsdb-client backup\""
+            echo "save $savedir/switch.summary $subnet \"ovs-vsctl show\""
 
             l2_cur[$l2_name]=$((${l2_cur[$l2_name]}+1))
-        fi
-    done
+        done
 
-    echo "" >> $file_loc
-    echo "tar -czf \${dirname}.tar.gz \${dirname}/*" >> $file_loc
-    echo "echo \"Download the archive file:\"" >> $file_loc
-    echo "echo \"    scp -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname}.tar.gz .\"" >> $file_loc
-    echo "echo 'Extract the archive:'" >> $file_loc
-    echo "echo \"    tar -xzf \${dirname}.tar.gz\"" >> $file_loc
-    echo "echo \"Overwrite the config folder in the current directory:\"" >> $file_loc
-    echo "echo \"    scp -r -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname} config\"" >> $file_loc
-    echo "echo ''" >> $file_loc
-    echo "echo 'If the scp commands do not work for you, use ssh:'" >> $file_loc
-    echo "echo '(Reliable only on UNIX systems. On Windows, you may use WinSCP instead)'" >> $file_loc
-    echo "echo \"Download the archive:\"" >> $file_loc
-    echo "echo \"    ssh -q -p $((2000 + ${group_number})) root@duvel.ethz.ch cat \${dirname}.tar.gz > \${dirname}.tar.gz\"" >> $file_loc
-    echo "echo \"Download and unpack the archive:\"" >> $file_loc
-    echo "echo \"    ssh -q -p $((2000 + ${group_number})) root@duvel.ethz.ch cat \${dirname}.tar.gz | tar -xz\"" >> $file_loc
+        for ((l=0;l<n_l2_hosts;l++)); do
+            host_l=(${l2_hosts[$l]})
+            hname="${host_l[0]}"
+            if [[ "$hname" != *VPN* ]];then
+                l2_name="${host_l[2]}"
+                subnet=$(subnet_sshContainer_groupContainer "${group_number}" "${l2_id[$l2_name]}" "${l2_cur[$l2_name]}" "L2-host")
+                savedir="\${dirname}/${hname}"
+
+                echo "echo ${hname}"
+                echo "mkdir -p $savedir"
+                echo "save $savedir/host.ip     $subnet \"ip addr\""
+                echo "save $savedir/host.route  $subnet \"ip route\""
+                echo "save $savedir/host.route6 $subnet \"ip -6 route\""
+
+                l2_cur[$l2_name]=$((${l2_cur[$l2_name]}+1))
+            fi
+        done
+    } >> $file_loc
+
+    {
+        echo ""
+        echo "tar -czf \${dirname}.tar.gz \${dirname}/*"
+        echo ""
+        echo "echo 'Saving complete!'"
+        echo "echo ''"
+        echo "echo \"Download the archive file (run these commands from your own computer):\""
+        echo "echo \"    scp -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname}.tar.gz .\""
+        echo "echo 'Extract the archive:'"
+        echo "echo \"    tar -xzf \${dirname}.tar.gz\""
+        echo "echo \"Overwrite the config folder in the current directory:\""
+        echo "echo \"    scp -r -P $((2000 + ${group_number})) root@duvel.ethz.ch:\${dirname} config\""
+        echo "echo ''"
+        echo "echo 'If the scp commands do not work for you, use ssh (also from your own computer):'"
+        echo "echo '(Reliable only on UNIX systems. On Windows, you may use WinSCP instead)'"
+        echo "echo \"Download the archive:\""
+        echo "echo \"    ssh -q -p $((2000 + ${group_number})) root@duvel.ethz.ch cat \${dirname}.tar.gz > \${dirname}.tar.gz\""
+        echo "echo \"Download and unpack the archive:\""
+        echo "echo \"    ssh -q -p $((2000 + ${group_number})) root@duvel.ethz.ch cat \${dirname}.tar.gz | tar -xz\""
+        echo "echo ''"
+        echo "echo 'If you are using an ssh config file, you may need to update the ssh commands above to match your configuration.'"
+        echo "echo 'Contact the TAs if you are unable to download your files!'"
+    } >> $file_loc
 done
