3a18e8c238f97a9d92deafe09f3b75c5201bc098
CommNet Team <nsg@ethz.ch>
2024-03-25 20:03:19 +0100

    Update variables to be publish-friendly :)

--- platform/config/variables.sh
+++ platform/config/variables.sh
@@ -16,8 +16,8 @@ DOCKERHUB_PREFIX="miniinterneteth/"
 # You need to specify the hostname of the server and an email for
 # LetsEncrypt to be enabled.
 # UPDATE THOSE VARIABLES. WEBSERVER_HOSTNAME -> hostname of the server and EMAIL -> empty string (for http)
-WEBSERVER_HOSTNAME="duvel.ethz.ch"
-WEBSERVER_ACME_MAIL="nsg@ethz.ch"
+WEBSERVER_HOSTNAME="localhost"
+WEBSERVER_ACME_MAIL=""
 # Hostname and ports for the webserver and krill on the host.
 # (must be publicly available)
 # you can change http and https ports, but letsencrypt won't work, so its not recommended.
@@ -57,17 +57,18 @@ HISTORY_ENABLED=true
 # Can reduce disk usage when the mini internet is not used immediately.
 HISTORY_PAUSE_AFTER_START=false
 # How often to fetch update configs and matrix state.
-HISTORY_UPDATE_FREQUENCY=$(( 60*60 ))  # every hour
+HISTORY_UPDATE_FREQUENCY=$(( 60*60 ))  # every hours (in seconds)
 # Timeout for ./save_configs.sh
 HISTORY_TIMEOUT="300s"
 # Username and email that will show up on the commit.
 HISTORY_GIT_USER="Mini-Internet History"
-HISTORY_GIT_EMAIL="nsg@ethz.ch"
+HISTORY_GIT_EMAIL=""
 # URL of the git repository to push the snapshots to; should be accessible.
 # For example, create a gitlab access token and use it in the URL.
 # The token needs to have write access to the repository.
 # HISTORY_GIT_URL="https://gitlab-ci-token:<TOKEN HERE>@gitlab.ethz.ch/nsg/lectures/lec_commnet/projects/2024/routing_project/test_history.git"
-HISTORY_GIT_URL="https://gitlab-ci-token:MH-jPMMjS_az8FqyiSBV@gitlab.ethz.ch/nsg/lectures/lec_commnet/projects/2024/routing_project/test_history.git"
+#HISTORY_GIT_URL="https://gitlab-ci-token:YOURTOKEN@gitlab.ethz.ch/YOURPROJECT.git"
+HISTORY_GIT_URL=""
 HISTORY_GIT_BRANCH="main"
 # switch.db and rpki.cache are binaries that cannot be stored easily in git.
 # If this option is "true" (recommend), we re-write the git history at every
